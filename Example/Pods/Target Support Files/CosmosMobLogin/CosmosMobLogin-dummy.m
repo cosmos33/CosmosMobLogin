5f9675c81c89f57260694266e815df2155b7af23
xtra <xtra@msn.c.n>
2023-09-19 01:08:53 -0700

    first commit

---
+++ Example/Pods/Target Support Files/CosmosMobLogin/CosmosMobLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CosmosMobLogin : NSObject
+@end
+@implementation PodsDummy_CosmosMobLogin
+@end
---
+++ Example/Pods/Target Support Files/Pods-CosmosMobLogin_Tests/Pods-CosmosMobLogin_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CosmosMobLogin_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CosmosMobLogin_Tests
+@end
