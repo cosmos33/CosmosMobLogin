f55ea9fa9a9e0a4219804bfc35e7e2dfc744cd83
xtra <xtra@msn.c.n>
2023-09-19 01:39:18 -0700

    add file

---
+++ CosmosMobLoginClient.h
@@ -0,0 +1,16 @@
+//
+//  CosmosMobLoginClient.h
+//  CosmosMobLogin
+//
+//  Created by 陈国贤 on 2023/9/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CosmosMobLoginClient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CosmosMobLoginClient.m
@@ -0,0 +1,12 @@
+//
+//  CosmosMobLoginClient.m
+//  CosmosMobLogin
+//
+//  Created by 陈国贤 on 2023/9/19.
+//
+
+#import "CosmosMobLoginClient.h"
+
+@implementation CosmosMobLoginClient
+
+@end
